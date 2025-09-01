720df6b42eaf8e962ad53000123a132fb95c1e9c
ana julia <ana.j2007@aluno.ifsc.edu.br>
2025-09-01 09:03:16 -0300

    sorteio

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.meuapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText editTextMax, editTextMin;
+    TextView tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        button = findViewById(R.id.button);
+        editTextMin = findViewById(R.id.edMin);
+        editTextMax = findViewById(R.id.edMax);
+        tv=findViewById(R.id.tvResultado);
+        button.setOnClickListener( v -> {
+
+            Random random=new Random();
+            int min, max;
+            min=Integer.parseInt(editTextMin.getText().toString());
+            max=Integer.parseInt(editTextMax.getText().toString());
+            int delta=max-min;
+            int sorteado= random.nextInt(delta)+min;
+            tv.setText(Integer.toString(sorteado));
+        });
+
     }
 }
